1efb02ae70435e1fbad3afc0200f759ddd5a2842
Wilson Costa <66930727+Wilson-cmd@users.noreply.github.com>
2024-02-17 10:32:37 -0300

    Update jpg-to-png.sh
    Variable PATHIMAGES created to better reference

--- jpg-to-png.sh
+++ jpg-to-png.sh
@@ -1,9 +1,10 @@
 #!/bin/bah
 #Convert jpg to png
-for image in $(ls ~/imagens-livros/ | sed "s/.jpg//")
+PATHIMAGES=~/imagens-livros
+for image in $(ls $PATHIMAGES/ | sed "s/.jpg//")
 do
-convert ~/imagens-livros/"$image.jpg" ~/imagens-livros/"$image.png"
+convert $PATHIMAGES/"$image.jpg" $PATHIMAGES/"$image.png"
 done
 
 mkdir -p ~/png
-mv  ~/imagens-livros/*.png ~/png/
+mv  $PATHIMAGES/*.png ~/png/
